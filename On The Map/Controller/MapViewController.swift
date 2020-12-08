f4c008023137cf0325ae0a3e398f00c7fe20d21f
Tommy Lam <>
2020-12-08 15:05:21 -0500

    added map kit into map vC

--- On The Map/Controller/MapViewController.swift
+++ On The Map/Controller/MapViewController.swift
@@ -6,9 +6,11 @@
 //
 
 import UIKit
+import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
